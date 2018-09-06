80a9a5b4ec1ee4534fc6be3e0fe2e998876d849c
Marc Ablay <marc.ablay@blockchain-consulting.net>
2018-09-06 08:32:19 +0200

    fixed postinstall script to actually execute

--- keystore/init.sh
+++ keystore/init.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-FILE="server.crt"
-if [ ! -f $FILE ]; then
-  echo "File $FILE already exists."
+KEY="keystore/server.key"
+CRT="keystore/server.crt"
+if [ -f $KEY ]; then
+  echo "File $KEY already exists."
 else
-  openssl req -x509 -out server.crt -keyout server.key \
+  openssl req -x509 -out $CRT -keyout $KEY \
   -newkey rsa:2048 -nodes -sha256 \
   -subj '/CN=localhost' -extensions EXT -config <( \
   printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
